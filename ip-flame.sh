aa00fb3bc34b2d416ff14df8eb6fa3f66f4605c1
Trevelyn412 <weaknetlabs@gmail.com>
2015-10-21 19:36:20 +0000

    Updated script to show counter

--- ip-flame.sh
+++ ip-flame.sh
@@ -11,23 +11,26 @@ offlines=$(egrep '^offending-lines' config-ip-flame.txt|sed -re 's/[^=]+=//')
 printf "[ + ] Matching offending lines: $offlines\n"
 # dump in top of HTML page, this initiates and overwrites:
 cat templates/top.html > flamed-ips.html
-shodan="<a title='Search Shodan for devices hosted by {}' href='https://www.shodan.io/search?query="
-shodan2="' target='_blank'>Shodan.io Data</a>"
-spamcop="<a title='SpamCop.net - report {} for Spam violation' target='_blank' href='https://www.spamcop.net/w3m?action=checkblock&ip="
-spamcop2="'>SpamCop.net Data</a>"
-infobyip="<a title='Detailed information for {}' href='http://www.infobyip.com/ip-"
-infobyip2=".html' target='_blank'>InfoByIp.com Data</a>"
+shodan="<a title='Search Shodan for devices hosted by $ip' href='https://www.shodan.io/search?query="
+shodan2="' target='_blank'>Shodan</a>"
+spamcop="<a title='SpamCop.net - report $ip for Spam violation' target='_blank' href='https://www.spamcop.net/w3m?action=checkblock&ip="
+spamcop2="'>SpamCop</a>"
+infobyip="<a title='Detailed information for $ip' href='http://www.infobyip.com/ip-"
+infobyip2=".html' target='_blank'>InfoByIp</a>"
 sophos="<a title='Sophos Security' href='https://www.sophos.com/en-us/threat-center/ip-lookup.aspx?ip="
-sophos2="' target='_blank'>Sophos.com Data</a>"
-arin="<a title='American Registry for Internet Numbers - {}' target='_blank' href='http://whois.arin.net/rest/nets;q="
+sophos2="' target='_blank'>Sophos</a>"
+arin="<a title='American Registry for Internet Numbers - $ip' target='_blank' href='http://whois.arin.net/rest/nets;q="
 arin2="?showDetails=true&showARIN=false&showNonArinTopLevelNet=false&ext=netref2'>ARIN</a>"
-apnic="<a title='APNIC JSON Data for {}' target='_blank' href='https://wq.apnic.net/whois-search/query?searchtext="
+apnic="<a title='APNIC JSON Data for $ip' target='_blank' href='https://wq.apnic.net/whois-search/query?searchtext="
 apnic2="'>APNIC</a>"
 # run command:
-egrep "$offlines" $log | \
-	awk '{print $1}'|egrep -v "($whitelist)"| \
-	sort -u|xargs -I {} \
-	echo -e "\t\t <tr><td>{}</td><td>$arin{}$arin2</td><td>$apnic{}$apnic2</td><td>$infobyip{}$infobyip2</td><td>$spamcop{}$spamcop2</td><td>$sophos{}$sophos2</td><td>$shodan{}$shodan2</td></tr>" >> flamed-ips.html
+n=0; # record counting
+lines=($(egrep "$offlines" $log | awk '{print $1}'|egrep -v "($whitelist)"| sort -u))
+for ip in "${lines[@]}"
+	do
+		echo -e "\t\t <tr><td class='count'>$n</td><td>$ip</td><td>$arin$ip$arin2</td><td>$apnic$ip$apnic2</td><td>$infobyip$ip$infobyip2</td><td>$spamcop$ip$spamcop2</td><td>$sophos$ip$sophos2</td><td>$shodan$ip$shodan2</td></tr>" >> flamed-ips.html
+		n=$((n+1)) # expansion and increment
+	done;
 # dump in bottom of HTML page:
 cat templates/bottom.html >> flamed-ips.html
 cp flamed-ips.html $path
