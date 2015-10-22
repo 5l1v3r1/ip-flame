ca83fe3151257db7e0d36810b496b19b4263e442
Trevelyn412 <weaknetlabs@gmail.com>
2015-10-22 12:32:00 +0000

    Layout changed, updated configuration file

--- ip-flame.sh
+++ ip-flame.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+# IP-Flame - Hack/Spam/DoS Attempt IP Wall-of-Shame generator
+# 2015 - WeakNet Labs
+# Douglas Berdeaux (weaknetlabs@gmail.com)
+#
+#
 # create whitelist (CSV) from whitelist.txt:
-# using AWK and SED because QAWK programming is about as ugly as Perl with sigils.
 log=$(egrep '^web-log' config-ip-flame.txt|sed -re 's/[^=]+=//')
 printf "[ + ] WWW log location: $log\n"
 path="$(egrep '^web-path' config-ip-flame.txt|sed -re 's/[^=]+=//')flamed-ips.html"
-printf "[ + ] Using web-host path: $path\n"
+printf "[ + ] Using web-host path: $path\n" # whitelist of IP addresses to not "flame" on the wall:
 whitelist=$(awk -vORS='' 'BEGIN {i=0}{if($1 ~ /^[0-9]/) a[$i]=$1 }END{for(j in a){print j"|"}}' whitelist.txt|sed -re 's/\|$//')
 printf "[ + ] Generated IP white list: $whitelist\n"
-offlines=$(egrep '^offending-lines' config-ip-flame.txt|sed -re 's/[^=]+=//')
-printf "[ + ] Matching offending lines: $offlines\n"
-# dump in top of HTML page, this initiates and overwrites:
-cat templates/top.html > flamed-ips.html
+offlines=$(egrep '^offending-lines' config-ip-flame.txt|sed -re 's/[^=]+=//') # offending lines, can be regular expressions
+printf "[ + ] Matching offending lines: $offlines\n" 
+tmppath=$(egrep '^tmp-path' config-ip-flame.txt|sed -re 's/[^=]+=//') # temporary working directory
+# These "global" strings, just make the actual for loop below MUCH more readable:
 shodan="<a title='Search Shodan for devices hosted by $ip' href='https://www.shodan.io/search?query="
 shodan2="' target='_blank'>Shodan</a>"
 spamcop="<a title='SpamCop.net - report $ip for Spam violation' target='_blank' href='https://www.spamcop.net/w3m?action=checkblock&ip="
@@ -25,13 +29,18 @@ apnic="<a title='APNIC JSON Data for $ip' target='_blank' href='https://wq.apnic
 apnic2="'>APNIC</a>"
 # run command:
 n=0; # record counting
+# now get all of the lines from the log file (this may take a while!):
 lines=($(egrep "$offlines" $log | awk '{print $1}'|egrep -v "($whitelist)"| sort -u))
+# dump in top of HTML page, this initiates and overwrites:
+cat templates/top.html > $tmppath/flamed-ips.html
 for ip in "${lines[@]}"
-	do
-		echo -e "\t\t <tr><td class='count'>$n</td><td>$ip</td><td>$arin$ip$arin2</td><td>$apnic$ip$apnic2</td><td>$infobyip$ip$infobyip2</td><td>$spamcop$ip$spamcop2</td><td>$sophos$ip$sophos2</td><td>$shodan$ip$shodan2</td></tr>" >> flamed-ips.html
+	do # now we generate the line and append it to the HTML file:
+		echo -e "\t\t <tr><td class='count'>$n</td><td>$ip</td><td>$arin$ip$arin2</td><td>$apnic$ip$apnic2</td><td>$infobyip$ip$infobyip2</td><td>$spamcop$ip$spamcop2</td><td>$sophos$ip$sophos2</td><td>$shodan$ip$shodan2</td></tr>" >> $tmppath/flamed-ips.html
 		n=$((n+1)) # expansion and increment
 	done;
 # dump in bottom of HTML page:
-cat templates/bottom.html >> flamed-ips.html
-cp flamed-ips.html $path
+cat templates/bottom.html >> $tmppath/flamed-ips.html
+# generate ".summary" top left div:
+echo "<div class='summary'><table><tr><td>Report Generated: $(date)</td><td>Offenders: $n</td><td>Offending String: \"$offlines\"</td></tr></table></div>" >> $tmppath/flamed-ips.html
+cp $tmppath/flamed-ips.html $path
 printf "[ + ] HTML file generation completed and stored in $path\n"
